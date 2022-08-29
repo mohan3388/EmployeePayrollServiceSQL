7a57e9fc9ba93b136518ac4a451122dbfc30eb45
Mohan Sahu <68227271+mohan3388@users.noreply.github.com>
2022-08-29 12:10:52 -0700

    Update CreateDatabase.sql

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -3,3 +3,12 @@
 create database payroll_service
 
 use payroll_service
+
+-------UC2-------
+create table employee_payroll
+(
+ Id int Identity,
+ Name varchar(50),
+ Salary int,
+ StartDate date
+);
